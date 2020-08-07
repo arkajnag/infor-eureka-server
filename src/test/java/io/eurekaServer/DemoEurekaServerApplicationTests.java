feaca07b8a78d1de1c0f593ab5a376a9cd94989b
arkajnag <arka.imps@gmail.com>
2020-08-07 07:18:46 +0200

    Commiting Eureka Service Discovery Server code from Local Repo to Remote Repo

---
+++ src/test/java/io/eurekaServer/DemoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
